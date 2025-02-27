627dc8c23a9d15b72dbd8e2068452457f0c6c36a
le.luo <le.luo@envision-digital.com>
2025-02-27 14:56:25 +0800

    feat: 删除不必要的文件

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>我的 React 应用</h1>
+        <p>开始编写您的应用</p>
       </header>
     </div>
   );
